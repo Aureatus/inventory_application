042de1f04551d92d7ef8df59ff855913ae9da3e2
Alex Knott <alextknott@gmail.com>
2022-09-07 16:41:25 +0100

    Implement categoriesRouter as middleware on path /categories

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+const categoriesRouter = require("./routes/categories");
 
 var app = express();
 // Connect to mongoDB
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/categories", categoriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
