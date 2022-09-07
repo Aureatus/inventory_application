dde3b41a3a543be02bb975e6e47790aa508246ca
Alex Knott <alextknott@gmail.com>
2022-09-07 16:59:19 +0100

    Create router to handle item routes

---
+++ routes/item.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/:id/create", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.post("/:id/create", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.get("/:id/delete", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.post("/:id/delete", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.get("/:id/update", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.post("/:id/update", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+router.get("/:id/", (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
+
+module.exports = router;
