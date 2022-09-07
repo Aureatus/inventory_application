f35d69e37c45b86fde6534a09618774a34c1e608
Alex Knott <alextknott@gmail.com>
2022-09-07 18:06:21 +0100

    Implement controllers as callback for routes

--- routes/item.js
+++ routes/item.js
@@ -1,33 +1,29 @@
 const { Router } = require("express");
 
+const {
+  item_detail,
+  item_create_get,
+  item_create_post,
+  item_delete_get,
+  item_delete_post,
+  item_update_get,
+  item_update_post,
+} = require("../controllers/itemController");
+
 const router = Router();
 
-router.get("/:id/create", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.get("/:id/create", item_create_get);
 
-router.post("/:id/create", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.post("/:id/create", item_create_post);
 
-router.get("/:id/delete", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.get("/:id/delete", item_delete_get);
 
-router.post("/:id/delete", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.post("/:id/delete", item_delete_post);
 
-router.get("/:id/update", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.get("/:id/update", item_update_get);
 
-router.post("/:id/update", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.post("/:id/update", item_update_post);
 
-router.get("/:id/", (req, res) => {
-  res.send("NOT IMPLEMENTED");
-});
+router.get("/:id/", item_detail);
 
 module.exports = router;
