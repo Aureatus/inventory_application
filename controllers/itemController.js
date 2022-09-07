050f97c31fc813040f4844b84ea6fe2d8a3a1af8
Alex Knott <alextknott@gmail.com>
2022-09-07 18:06:11 +0100

    Create controller for item with placeholder functions

---
+++ controllers/itemController.js
@@ -0,0 +1,29 @@
+const item = require("../models/item");
+
+exports.item_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.item_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
