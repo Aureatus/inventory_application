866036f62f08ec898191d85aaf76334f63a92b4d
Alex Knott <alextknott@gmail.com>
2022-09-08 13:54:21 +0100

    Implement functionality for category_detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 const category = require("../models/category");
+const item = require("../models/item");
+const async = require("async");
 
 exports.category_list = (req, res) => {
   category
@@ -17,7 +19,32 @@ exports.category_list = (req, res) => {
 };
 
 exports.category_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+  async.parallel(
+    {
+      Category(callback) {
+        category.findById(req.params.id).exec(callback);
+      },
+      Category_items(callback) {
+        item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("category_items", {
+        title: "Category Items",
+        category: results.Category,
+        category_items: results.Category_items,
+      });
+    }
+  );
 };
 
 exports.category_create_get = (req, res) => {
