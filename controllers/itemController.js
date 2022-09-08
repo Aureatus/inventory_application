77fc9c419a0ebbf54366bf0f01bb940155f4a24c
Alex Knott <alextknott@gmail.com>
2022-09-08 17:45:49 +0100

    Implement controller functions for updating items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,6 @@
+const category = require("../models/category");
 const item = require("../models/item");
+const async = require("async");
 
 exports.item_detail = (req, res) => {
   item.findById(req.params.id).exec((err, show_item) => {
@@ -41,9 +43,43 @@ exports.item_delete_post = (req, res) => {
 };
 
 exports.item_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+  async.parallel(
+    {
+      Item(callback) {
+        item.findById(req.params.id).populate("category").exec(callback);
+      },
+      Categories(callback) {
+        category.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("item_update_form", {
+        title: "Update item",
+        item: results.Item,
+        categories: results.Categories,
+      });
+    }
+  );
 };
 
 exports.item_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+  const Item = new item({
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    price: req.body.price,
+    number_in_stock: req.body.number_in_stock,
+    _id: req.params.id,
+  });
+
+  item.findByIdAndUpdate(req.params.id, Item, (err, Item) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect(Item.url);
+  });
 };
