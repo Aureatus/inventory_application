ac6757b252dc6f883b9c83dbf975b7692348c90e
Alex Knott <alextknott@gmail.com>
2022-09-08 17:17:11 +0100

    Change category so that the object is no longer contained by an array

--- models/item.js
+++ models/item.js
@@ -3,7 +3,7 @@ const { Schema, model } = require(`mongoose`);
 const itemSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  category: [{ type: Schema.Types.ObjectId, ref: "category" }],
+  category: { type: Schema.Types.ObjectId, ref: "category" },
   price: { type: Number, required: true },
   number_in_stock: { type: Number, required: true },
 });
