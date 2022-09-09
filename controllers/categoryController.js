893d283ed8792dd9f09b3cbaf7481b6368f23ec7
Alex Knott <alextknott@gmail.com>
2022-09-09 15:30:32 +0100

    Implement get and post functions for deleting category and it's items

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -67,12 +67,46 @@ exports.category_create_post = (req, res, next) => {
   });
 };
 
-exports.category_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+exports.category_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      Category(callback) {
+        category.findById(req.params.id).exec(callback);
+      },
+      Category_items(callback) {
+        item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("category_delete", {
+        category: results.Category,
+        category_items: results.Category_items,
+      });
+    }
+  );
 };
 
-exports.category_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+exports.category_delete_post = (req, res, next) => {
+  item.deleteMany({ category: req.params.id }).exec((err) => {
+    if (err) {
+      return next(err);
+    }
+    category.findByIdAndRemove(req.params.id, () => {
+      if (err) {
+        return next(err);
+      }
+    });
+    res.redirect("/categories");
+  });
 };
 
 exports.category_update_get = (req, res, next) => {
